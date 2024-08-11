d15655d18cc43b41ee9ef38155e6342fd74afa8a
SeekerDev <albertbaisa@gmail.com>
2024-08-11 18:52:07 +0800

    commit remove history url

--- src/frontend/util/components/changeWindowHistory.js
+++ src/frontend/util/components/changeWindowHistory.js
@@ -1,7 +1,6 @@
 function changeWindowHistory(id) {
   const section = document.getElementById(id);
   section.scrollIntoView({ behavior: "smooth", block: "nearest" });
-  window.history.pushState(null, "/", id);
 }
 
 export default changeWindowHistory;
